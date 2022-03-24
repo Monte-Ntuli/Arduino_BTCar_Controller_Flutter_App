3bbbf40864c25eb7341437b8334f71e94d882e1e
Monte-Ntuli <86322309+Monte-Ntuli@users.noreply.github.com>
2022-03-24 16:48:26 +0200

    first commit

---
+++ lib/led.dart
@@ -0,0 +1,272 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:control_button/control_button.dart';
+
+class ChatPage extends StatefulWidget {
+  final BluetoothDevice server;
+
+  const ChatPage({this.server});
+
+  @override
+  _ChatPage createState() => new _ChatPage();
+}
+
+class _Message {
+  int whom;
+  String text;
+
+  _Message(this.whom, this.text);
+}
+
+class _ChatPage extends State<ChatPage> {
+  static final clientID = 0;
+  BluetoothConnection connection;
+
+  List<_Message> messages = List<_Message>();
+  String _messageBuffer = '';
+
+  final TextEditingController textEditingController =
+      new TextEditingController();
+  final ScrollController listScrollController = new ScrollController();
+
+  bool isConnecting = true;
+  bool get isConnected => connection != null && connection.isConnected;
+
+  bool isDisconnecting = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    BluetoothConnection.toAddress(widget.server.address).then((_connection) {
+      print('Connected to the device');
+      connection = _connection;
+      setState(() {
+        isConnecting = false;
+        isDisconnecting = false;
+      });
+
+      connection.input.listen(_onDataReceived).onDone(() {
+        // Example: Detect which side closed the connection
+        // There should be `isDisconnecting` flag to show are we are (locally)
+        // in middle of disconnecting process, should be set before calling
+        // `dispose`, `finish` or `close`, which all causes to disconnect.
+        // If we except the disconnection, `onDone` should be fired as result.
+        // If we didn't except this (no flag set), it means closing by remote.
+        if (isDisconnecting) {
+          print('Disconnecting locally!');
+        } else {
+          print('Disconnected remotely!');
+        }
+        if (this.mounted) {
+          setState(() {});
+        }
+      });
+    }).catchError((error) {
+      print('Cannot connect, exception occured');
+      print(error);
+    });
+  }
+
+  @override
+  void dispose() {
+    // Avoid memory leak (`setState` after dispose) and disconnect
+    if (isConnected) {
+      isDisconnecting = true;
+      connection.dispose();
+      connection = null;
+    }
+
+    super.dispose();
+  }
+
+  double _value = 90.0;
+  @override
+  Widget build(BuildContext context) {
+    final List<Row> list = messages.map((_message) {
+      return Row(
+        children: <Widget>[
+          Container(
+            child: Text(
+                (text) {
+                  return text == '/shrug' ? '¯\\_(ツ)_/¯' : text;
+                }(_message.text.trim()),
+                style: TextStyle(color: Colors.white)),
+            padding: EdgeInsets.all(12.0),
+            margin: EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
+            width: 222.0,
+            decoration: BoxDecoration(
+                color: _message.whom == clientID
+                    ? Colors.greenAccent
+                    : Colors.grey,
+                borderRadius: BorderRadius.circular(7.0)),
+          ),
+        ],
+        mainAxisAlignment: _message.whom == clientID
+            ? MainAxisAlignment.end
+            : MainAxisAlignment.start,
+      );
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+          title: (isConnecting
+              ? Text('Connecting to ' + widget.server.name + '...')
+              : isConnected
+                  ? Text('Connected to ' + widget.server.name)
+                  : Text('Now Controlling ' + widget.server.name))),
+      body: Container(
+          child: Stack(children: <Widget>[
+        Align(
+          alignment: Alignment(-0.7, -0.8),
+          child: RaisedButton(
+            child: Text("Forward"),
+            color: Colors.blue,
+            onPressed: () {
+              _sendMessage('8');
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(-0.9, -0.2),
+          child: RaisedButton(
+            child: Text("Left"),
+            color: Colors.blue,
+            onPressed: () {
+              _sendMessage('4');
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(-0.5, -0.2),
+          child: RaisedButton(
+            child: Text("Right"),
+            color: Colors.blue,
+            onPressed: () {
+              _sendMessage('6');
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(-0.7, 0.5),
+          child: RaisedButton(
+            child: Text("Backward"),
+            color: Colors.blue,
+            onPressed: () {
+              _sendMessage('2');
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(0.0, 0.0),
+          child: RaisedButton(
+            child: Text("STOP"),
+            color: Colors.red,
+            onPressed: () {
+              _sendMessage('5');
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(0.58, -0.5),
+          child: RaisedButton(
+            child: Text("Speed Up"),
+            color: Colors.blue,
+            onPressed: () {
+              _sendMessage('1');
+            },
+          ),
+        ),
+        Align(
+          alignment: Alignment(0.6, 0.5),
+          child: RaisedButton(
+            child: Text("Slow Down"),
+            color: Colors.blue,
+            onPressed: () {
+              _sendMessage('0');
+            },
+          ),
+        ),
+      ])),
+    );
+  }
+
+  void _onDataReceived(Uint8List data) {
+    // Allocate buffer for parsed data
+    int backspacesCounter = 0;
+    data.forEach((byte) {
+      if (byte == 8 || byte == 127) {
+        backspacesCounter++;
+      }
+    });
+    Uint8List buffer = Uint8List(data.length - backspacesCounter);
+    int bufferIndex = buffer.length;
+
+    // Apply backspace control character
+    backspacesCounter = 0;
+    for (int i = data.length - 1; i >= 0; i--) {
+      if (data[i] == 8 || data[i] == 127) {
+        backspacesCounter++;
+      } else {
+        if (backspacesCounter > 0) {
+          backspacesCounter--;
+        } else {
+          buffer[--bufferIndex] = data[i];
+        }
+      }
+    }
+
+    // Create message if there is new line character
+    String dataString = String.fromCharCodes(buffer);
+    int index = buffer.indexOf(13);
+    if (~index != 0) {
+      setState(() {
+        messages.add(
+          _Message(
+            1,
+            backspacesCounter > 0
+                ? _messageBuffer.substring(
+                    0, _messageBuffer.length - backspacesCounter)
+                : _messageBuffer + dataString.substring(0, index),
+          ),
+        );
+        _messageBuffer = dataString.substring(index);
+      });
+    } else {
+      _messageBuffer = (backspacesCounter > 0
+          ? _messageBuffer.substring(
+              0, _messageBuffer.length - backspacesCounter)
+          : _messageBuffer + dataString);
+    }
+  }
+
+  void _sendMessage(String text) async {
+    text = text.trim();
+    textEditingController.clear();
+
+    if (text.length > 0) {
+      try {
+        connection.output.add(utf8.encode(text + "\r\n"));
+        await connection.output.allSent;
+
+        setState(() {
+          messages.add(_Message(clientID, text));
+        });
+
+        Future.delayed(Duration(milliseconds: 333)).then((_) {
+          listScrollController.animateTo(
+              listScrollController.position.maxScrollExtent,
+              duration: Duration(milliseconds: 333),
+              curve: Curves.easeOut);
+        });
+      } catch (e) {
+        // Ignore error, but notify state
+        setState(() {});
+      }
+    }
+  }
+}
